f8fe5143b1a3ec091708289060038e591513e0bf
Stephan Lohwasser <me@lowi.one>
2022-02-14 16:05:51 +0100

    removce connection machine

--- src/machine/connection/connection.guards.ts
+++
@@ -1,7 +0,0 @@
-import type { ConnectionEvents } from './connection.events'
-import type { ConnectionContext } from './connection.machine'
-
-export default {
-    matchmakingOk: (_context: ConnectionContext, event: ConnectionEvents) =>
-        event.type === 'matchmaking_ok',
-}
